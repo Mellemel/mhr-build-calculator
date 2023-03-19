c2d244d4412b9fab21b4c376298f966b2fe58205
mellymel <sometotallyfakeemail@fakestemail.com>
2023-03-18 22:36:09 -0400

    added ImageCounter to armor and weapon row

--- src/components/ArmorRow.tsx
+++ src/components/ArmorRow.tsx
@@ -1,9 +1,17 @@
+import GearRow from './GearRow';
+import ImageCounter from './ImageCounter';
+
 import baseHeadArmor from '../images/armors/armour_head_r1.png';
 import baseChestArmor from '../images/armors/armour_chest_r1.png';
 import baseArmsArmor from '../images/armors/armour_arms_r1.png';
 import baseWaistArmor from '../images/armors/armour_waist_r1.png';
 import baseLegsArmor from '../images/armors/armour_legs_r1.png';
-import GearRow from './GearRow';
+import {ReactComponent as ElementalFireIcon } from '../images/elementals/elementalres_fire_icon.svg';
+import {ReactComponent as ElementalWaterIcon } from '../images/elementals/elementalres_water_icon.svg';
+import {ReactComponent as ElementalIceIcon } from '../images/elementals/elementalres_ice_icon.svg';
+import {ReactComponent as ElementalThunderIcon } from '../images/elementals/elementalres_thunder_icon.svg';
+import {ReactComponent as ElementalDragonIcon } from '../images/elementals/elementalres_dragon_icon.svg';
+import DefenseIcon from '../images/icons/mhw_screenshot_defense_icon_v0.png'
 
 type Armor = 'head' | 'chest' | 'arms' | 'waist' | 'legs';
 
@@ -15,12 +23,12 @@ function ArmorRow(props: ArmorRowProps) {
   return (
     <GearRow image={imageType[props.type]} title='N/A'>
       <div className='pure-g'>
-        <p>Defense</p>
-        <p>Fire</p>
-        <p>Water</p>
-        <p>Ice</p>
-        <p>Thunder</p>
-        <p>Dragon</p>
+        <ImageCounter icon={<img className='icon-container-svg' src={DefenseIcon} alt="Defense Icon" />}/>
+        <ImageCounter icon={<ElementalFireIcon className='icon-container-svg' />}/>
+        <ImageCounter icon={<ElementalWaterIcon className='icon-container-svg' />}/>
+        <ImageCounter icon={<ElementalIceIcon className='icon-container-svg' />}/>
+        <ImageCounter icon={<ElementalThunderIcon className='icon-container-svg' />}/>
+        <ImageCounter icon={<ElementalDragonIcon className='icon-container-svg' />}/>
       </div>
     </GearRow>
   )
