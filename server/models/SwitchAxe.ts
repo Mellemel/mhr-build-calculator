cbd5d4dc64127c2060ad12eb7b4768c533ff60b1
mellymel <sometotallyfakeemail@fakestemail.com>
2023-04-29 11:50:30 -0400

    added switchaxe model

---
+++ server/models/SwitchAxe.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Column, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Weapon } from "./Weapon";
+
+type PhialType = 'elemental' | 'exhaust' | 'dragon' | 'power' | 'paralysis';
+
+export class SwitchAxe extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column('simple-enum', {nullable: false})
+  phialType!: PhialType;
+  @Column()
+  phialValue: number | null = null;
+
+  @OneToOne(() => Weapon, {nullable: false})
+  weapon!: Weapon
+}
