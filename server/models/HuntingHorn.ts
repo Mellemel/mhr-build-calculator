0b963a0fc005b70effe7167e6272b2a8f7f7d0d9
mellymel <sometotallyfakeemail@fakestemail.com>
2023-04-29 11:38:39 -0400

    added huntinghorn model

---
+++ server/models/HuntingHorn.ts
@@ -0,0 +1,13 @@
+import { BaseEntity, Column, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Weapon } from "./Weapon";
+
+export class HuntingHorn extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column('simple-array')
+  songs: string[] = [];
+
+  @OneToOne(() => Weapon, {nullable: false})
+  weapon!: Weapon
+}
