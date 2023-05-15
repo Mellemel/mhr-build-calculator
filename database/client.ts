434faeda31579297e17a4b3ae59a7c8c342f7ff1
mellymel <sometotallyfakeemail@fakestemail.com>
2023-05-15 14:36:32 -0400

    dynamically loading in entities

--- database/client.ts
+++ database/client.ts
@@ -1,18 +1,17 @@
 import { DataBase } from ".";
-import { getClientDBConfig } from "./configuration";
-// tslint:disable-next-line
-// import sqlWasm from "sql.js/dist/sql-wasm.wasm";
+import { getClientDBConfig, loadEntities } from "./configuration";
+// @ts-expect-error
+import sqlWasm from "sql.js/dist/sql-wasm.wasm";
 import initSqlJs from 'sql.js';
 
 export class ClientDatabase extends DataBase {
   public static async initialize() {
-    console.log("Initializing client database", `${process.env.PUBLIC_URL}/mhr-database.sqlite`);
     const data = await fetch(`${process.env.PUBLIC_URL}/mhr-database.sqlite`);
     const dataBuffer = await data.arrayBuffer();
     const database = new Uint8Array(dataBuffer);
-    const driver = await initSqlJs();
-    const config = getClientDBConfig(database, driver);
-    console.log(config);
+    const driver = await initSqlJs({ locateFile: () => sqlWasm });
+    const entities = await loadEntities();
+    const config = getClientDBConfig(database, driver, entities);
     await super.initialize(config);
   }
 }
--- database/configuration.ts
+++ database/configuration.ts
@@ -1,26 +1,34 @@
 import { SqlJsStatic } from "sql.js";
-import { DataSourceOptions } from "typeorm";
-
+import { BaseEntity, DataSourceOptions, EntitySchema, MixedList } from "typeorm";
 
 export const dbConfig: DataSourceOptions = {
   type: "sqlite",
   database: "../public/mhr-database.sqlite",
   synchronize: true,
   logging: true,
-  entities: ["server/models/**/*.ts"],
-  migrations: ["server/migrations/**/*.ts"],
-  subscribers: ["server/subscribers/**/*.ts"],
+  entities: ["models/**/*.ts"],
 };
 
-export const getClientDBConfig = (database: Uint8Array, driver: SqlJsStatic): DataSourceOptions => {
+export const getClientDBConfig = (database: Uint8Array, driver: SqlJsStatic, entities: MixedList<string | Function | EntitySchema<any>>): DataSourceOptions => {
   return {
     type: "sqljs",
     database,
     driver,
-    entities: ["server/models/**/*.ts"],
-    migrations: ["server/migrations/**/*.ts"],
-    subscribers: ["server/subscribers/**/*.ts"],
     logging: true,
     synchronize: true,
+    entities,
   }
+}
+
+export async function loadEntities(): Promise<any[]> {
+  const entityContext = require.context('./models', true, /\.ts$/);
+  const entityPaths = entityContext.keys();
+
+  const entities = await Promise.all(
+    entityPaths.map(async (entityPath: string) => {
+      const module = await import(`./models/${entityPath.slice(2)}`);
+      return Object.values(module).filter((entity: any) => entity.prototype instanceof BaseEntity);
+    })
+  );
+  return entities.flat();
 }
